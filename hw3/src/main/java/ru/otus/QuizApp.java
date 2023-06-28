62087beaae5000a0950e5daf80a04e43426785de
Артём Семёнов <a.semenov@arenadc.io>
2023-06-28 16:00:40 +0700

    refactoring

--- hw3/src/main/java/ru/otus/QuizApp.java
+++ hw3/src/main/java/ru/otus/QuizApp.java
@@ -3,12 +3,9 @@ package ru.otus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Locale;
-
 @SpringBootApplication
 public class QuizApp {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.forLanguageTag("ru-RU"));
         SpringApplication.run(QuizApp.class, args);
     }
 }
