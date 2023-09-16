00dddf5de523efe01d9583457ed6b3febb6d5b14
Артём Семёнов <a.semenov@arenadc.io>
2023-09-16 21:15:49 +0300

    hw 10 files

---
+++ hw11/src/main/java/ru/otus/spring/service/CommentServiceImpl.java
@@ -0,0 +1,36 @@
+package ru.otus.spring.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.spring.repository.CommentRepository;
+import ru.otus.spring.domain.Comment;
+
+@Component
+@SuppressWarnings("unused")
+public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository repository;
+
+    public CommentServiceImpl(CommentRepository repository) {
+        this.repository = repository;
+    }
+
+    @Transactional
+    @Override
+    public Comment save(Comment comment) {
+        return repository.save(comment);
+    }
+
+    @Override
+    public Comment findById(long id) {
+        return repository.findById(id).orElseThrow(EntityNotFoundException::new);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(long id) {
+        repository.deleteById(id);
+    }
+
+}
