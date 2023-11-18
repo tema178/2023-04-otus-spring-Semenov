ae9dc2cc3b8aead17e6d48dba3a9c7f8d6f67167
Артём Семёнов <a.semenov@arenadc.io>
2023-11-18 12:42:17 +0300

    refactoring

--- hw13/src/test/java/ru/otus/spring/rest/authorization/AuthorRestControllerTest.java
+++ hw13/src/test/java/ru/otus/spring/rest/authorization/AuthorRestControllerTest.java
@@ -2,11 +2,14 @@ package ru.otus.spring.rest.authorization;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.otus.spring.domain.Author;
@@ -17,6 +20,7 @@ import ru.otus.spring.security.SecurityConfiguration;
 import ru.otus.spring.service.AuthorService;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -48,18 +52,20 @@ public class AuthorRestControllerTest {
                 .andExpect(redirectedUrl(LOGIN));
     }
 
-    @Test
-    void authorApiTestAuthorized() throws Exception {
-        given(authorService.getAll()).willReturn(List.of());
-        var users = List.of(user("user"), user("admin").authorities(
+    private static Stream<SecurityMockMvcRequestPostProcessors.UserRequestPostProcessor> getUsers() {
+        return Stream.of(user("user"), user("admin").authorities(
                 List.of(new SimpleAuthority("ADMIN"))));
-        for (var user : users) {
-            mockMvc.perform(MockMvcRequestBuilders.get("/api/author").with(user)).andExpect(status().isOk());
-            mockMvc.perform(MockMvcRequestBuilders.put("/api/author").with(user).with(csrf())
-                    .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isForbidden());
-            mockMvc.perform(MockMvcRequestBuilders.delete("/api/author/1").with(user).with(csrf())
-                    .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isForbidden());
-        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("getUsers")
+    void authorApiTestAuthorized(SecurityMockMvcRequestPostProcessors.UserRequestPostProcessor user) throws Exception {
+        given(authorService.getAll()).willReturn(List.of());
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/author").with(user)).andExpect(status().isOk());
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/author").with(user).with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isForbidden());
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/author/1").with(user).with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isForbidden());
     }
 
     @Test
--- hw13/src/test/java/ru/otus/spring/rest/authorization/GenreRestControllerTest.java
+++ hw13/src/test/java/ru/otus/spring/rest/authorization/GenreRestControllerTest.java
@@ -2,11 +2,14 @@ package ru.otus.spring.rest.authorization;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.otus.spring.domain.Genre;
@@ -17,6 +20,7 @@ import ru.otus.spring.security.SecurityConfiguration;
 import ru.otus.spring.service.GenreService;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -49,18 +53,20 @@ public class GenreRestControllerTest {
                 .andExpect(redirectedUrl(LOGIN));
     }
 
-    @Test
-    void genreApiTestAuthorized() throws Exception {
-        given(genreService.getAll()).willReturn(List.of());
-        var users = List.of(user("user"), user("admin").authorities(
+    private static Stream<SecurityMockMvcRequestPostProcessors.UserRequestPostProcessor> getUsers() {
+        return Stream.of(user("user"), user("admin").authorities(
                 List.of(new SimpleAuthority("ADMIN"))));
-        for (var user : users) {
-            mockMvc.perform(MockMvcRequestBuilders.get("/api/genre").with(user)).andExpect(status().isOk());
-            mockMvc.perform(MockMvcRequestBuilders.put("/api/genre").with(user).with(csrf())
-                    .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isForbidden());
-            mockMvc.perform(MockMvcRequestBuilders.delete("/api/genre/1").with(user).with(csrf())
-                    .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isForbidden());
-        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("getUsers")
+    void genreApiTestAuthorized(SecurityMockMvcRequestPostProcessors.UserRequestPostProcessor user) throws Exception {
+        given(genreService.getAll()).willReturn(List.of());
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/genre").with(user)).andExpect(status().isOk());
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/genre").with(user).with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isForbidden());
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/genre/1").with(user).with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isForbidden());
     }
 
     @Test
